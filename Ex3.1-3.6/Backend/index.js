d005c3730812ced5c09cd7c7fed82e44d37d94e8
Shubhanker01 <shubhanker40@gmail.com>
2025-05-15 16:15:07 +0530

    Adding origin to cors

--- Ex3.1-3.6/Backend/index.js
+++ Ex3.1-3.6/Backend/index.js
@@ -3,7 +3,9 @@ const app = express()
 const PORT = process.env.PORT || 3001
 const cors = require('cors')
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:['https://part-3-hazel.vercel.app/']
+}))
 
 let phoneBook = [
     {
