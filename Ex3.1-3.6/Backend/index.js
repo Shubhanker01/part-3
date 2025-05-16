6f1c843527d9fb68dda7a07fb1b6afd86c38112d
Shubhanker01 <shubhanker40@gmail.com>
2025-05-16 15:30:41 +0530

    Removed cors Backend

--- Ex3.1-3.6/Backend/index.js
+++ Ex3.1-3.6/Backend/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
-const cors = require('cors')
 app.use(express.json())
-app.use(cors())
+
 
 let phoneBook = [
     {
