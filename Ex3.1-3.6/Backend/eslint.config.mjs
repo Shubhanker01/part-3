fd2c7dff3c8d72f76324d11476398a34083dc568
Shubhanker01 <shubhanker40@gmail.com>
2025-05-21 17:04:50 +0530

    Adding linter

--- Ex3.1-3.6/Backend/eslint.config.mjs
+++ Ex3.1-3.6/Backend/eslint.config.mjs
@@ -1,10 +1,28 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  // use eslint recommended settings
+  js.configs.recommended,
+  {
+    // looks for all javascript in our project
+    files: ['**/*.js'],
+    languageOptions: {
+      // js in node project use commonjs system
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+    },
+  },
+]
